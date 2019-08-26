6ee876747696001b5f1b3a7acc53471a18ebf8c5
Nickolas De Dios <ndedios1@binghamton.edu>
2019-08-26 17:12:53 -0400

    still working

---
+++ assignment01/ComputerTester.java
@@ -0,0 +1,33 @@
+package assignment01;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+public class ComputerTester {
+	public static void main(String[] args) {
+	//TODO make 4 different Computer objects with different
+	//combinations of disk size (two more than a terabyte and two less)
+	//and for each make one an SSD drive and the other not.
+	//Then call System.out.println(comp); for each "comp" object
+	//and observe that there are small differences in how the computer
+	//information prints out.
+		try(var output =new PrintWriter(new FileOutputStream(
+			    new File("output.txt"), true /* true means append to file */))) {
+			output.println("\nTESTS FOR Computer.java:");
+      //more than terrabyte, SSD
+      Computer test1 = new Computer("HP", "Intel Core", 16,1200,true, 1000 );
+      System.out.println(comp);
+      //more than terrabyte, no SSD
+
+      //less than terrabyte, SSD
+
+      //less than terrabyte, no SSD
+      
+		//Copy all your lines above, add an extra "2" to every variable name
+		// and replace every System.out.print or System.out.println
+		// by output.print or output.println
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
