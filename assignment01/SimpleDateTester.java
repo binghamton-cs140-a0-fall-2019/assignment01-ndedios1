d780cbc228011afedfcde720407aa13fb4106267
Nickolas De Dios <ndedios1@binghamton.edu>
2019-08-28 17:54:27 -0400

    added all new files

---
+++ assignment01/SimpleDateTester.java
@@ -0,0 +1,37 @@
+package assignment01;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+public class ComputerOwnerTester {
+  public static void main(String[] args) {
+    try(var output =new PrintWriter(new FileOutputStream(
+			    new File("output.txt"), true /* true means append to file */))) {
+			output.println("\nTESTS FOR SimpleDate.java:");
+
+      SimpleDate date1 = new SimpleDate();
+      SimpleDate date2 = new SimpleDate();
+      SimpleDate date3 = new SimpleDate();
+      SimpleDate date4 = new SimpleDate();
+      SimpleDate date5 = new SimpleDate();
+
+      var test1 = date1.of(2000,10,22);
+      var test2 = date2.of(2001,10,22);
+      var test3 = date3.of(2000,10,25);
+      var test4 = date4.of(1999,10,22);
+      var test5 = date5.of(2000,10,20);
+
+      System.out.println(test1.before(test2)); //true
+      System.out.println(test1.before(test3)); //true
+      System.out.println(test1.before(test4)); //false
+      System.out.println(test1.before(test5)); //false
+
+      output.println(test1.before(test2)); //true
+      output.println(test1.before(test3)); //true
+      output.println(test1.before(test4)); //false
+      output.println(test1.before(test5)); //false
+    }
+  } catch (FileNotFoundException e) {
+    e.printStackTrace();
+  }
+}
--- assignment01/USAddressTester.java
+++ assignment01/USAddressTester.java
@@ -8,7 +8,21 @@ public class USAddressTester {
     try(var output =new PrintWriter(new FileOutputStream(
 			    new File("output.txt"), true /* true means append to file */))) {
 			output.println("\nTESTS FOR StreetUSAddress.java:");
+      StreetUSAddress home1 = new StreetUSAddress("123 Sesame Street", "456 Dream Lane", "Manhattan", "NY","98765");
+      StreetUSAddress home2 = new StreetUSAddress("4400 Vestal Parkway E", "", "Binghamton", "NY", "13901");
+      System.out.println(home1);
+      System.out.println(home2);
 
+      StreetUSAddress home12 = new StreetUSAddress("123 Sesame Street", "456 Dream Lane", "Manhattan", "NY","98765");
+      StreetUSAddress home22 = new StreetUSAddress("4400 Vestal Parkway E", "", "Binghamton", "NY", "13901");
+      output.println(home12);
+      output.println(home22);
+
+      //Copy all your lines above, add an extra "2" to every variable name
+  		// and replace every System.out.print or System.out.println
+  		// by output.print or output.println
+  		} catch (FileNotFoundException e) {
+  			e.printStackTrace();
   }
 
 }
